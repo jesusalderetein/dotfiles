6e5ce1c4b0e197a304ff39c4ad747bbe1aa62465
Jesus Kenji Alderete <jesusk8er@gmail.com>
2017-10-29 16:56:23 -0300

    Add git configurations

--- setup.sh
+++ setup.sh
@@ -3,11 +3,15 @@ unlink ~/.vimrc
 unlink ~/.vim
 unlink ~/.tmux.conf
 unlink ~/.zshrc
+unlink ~/.gitconfig
+unlink ~/.gitignore
 
 ln -s ~/dotfiles/vimrc ~/.vimrc
 ln -s ~/dotfiles/vim ~/.vim
 ln -s ~/dotfiles/tmux.conf ~/.tmux.conf
 ln -s ~/dotfiles/zshrc ~/.zshrc
+ln -s ~/dotfiles/gitconfig ~/.gitconfig
+ln -s ~/dotfiles/gitignore ~/.gitignore
 
 ### Install Vundle at first
 rm -rf ~/dotfiles/bundle
