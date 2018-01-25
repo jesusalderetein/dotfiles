09996a6808183b750838b83887967baee99ee839
Jesus Alderete <jesusk8er@gmail.com>
2018-01-24 22:51:37 -0300

    Add Tmux Plugin Manager + Tmux Yank

--- setup.sh
+++ setup.sh
@@ -1,23 +1,12 @@
 ### Symbolic links
-unlink ~/.vimrc
-unlink ~/.vim
-unlink ~/.tmux.conf
-unlink ~/.zshrc
-unlink ~/.gitconfig
-unlink ~/.gitignore
-unlink ~/.gvimrc
-
-ln -s ~/dotfiles/vimrc ~/.vimrc
-ln -s ~/dotfiles/vim ~/.vim
-ln -s ~/dotfiles/tmux.conf ~/.tmux.conf
-ln -s ~/dotfiles/zshrc ~/.zshrc
-ln -s ~/dotfiles/gitconfig ~/.gitconfig
-ln -s ~/dotfiles/gitignore ~/.gitignore
-ln -s ~/dotfiles/gvimrc ~/.gvimrc
-
-### Install Vundle at first
-rm -rf ~/dotfiles/bundle
-git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+ln -sf ~/dotfiles/vimrc ~/.vimrc
+ln -sf ~/dotfiles/vim ~/.vim
+ln -sf ~/dotfiles/tmux.conf ~/.tmux.conf
+ln -sf ~/dotfiles/tmux ~/.tmux
+ln -sf ~/dotfiles/zshrc ~/.zshrc
+ln -sf ~/dotfiles/gitconfig ~/.gitconfig
+ln -sf ~/dotfiles/gitignore ~/.gitignore
+ln -sf ~/dotfiles/gvimrc ~/.gvimrc
 
 ### Open vim and execute the Vundle
-vim -c "PluginInstall"
+vim -c "PlugInstall"
