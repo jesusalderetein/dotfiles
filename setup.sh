a6266852d5006b092367a3944d7e17a1037588ef
Jesus Alderete <jesusk8er@gmail.com>
2018-03-25 22:22:41 -0400

    Add gemrc dotfile

--- setup.sh
+++ setup.sh
@@ -7,6 +7,7 @@ ln -sf ~/dotfiles/zshrc ~/.zshrc
 ln -sf ~/dotfiles/gitconfig ~/.gitconfig
 ln -sf ~/dotfiles/gitignore ~/.gitignore
 ln -sf ~/dotfiles/gvimrc ~/.gvimrc
+ln -sf ~/dotfiles/gemrc ~/.gemrc
 
 ### Install git submodules
 git submodule update --init --recursive
